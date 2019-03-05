4d31530c9be178eacda21e5f660b9942f7992e0c
Administrator <Administrator@9HE79S4JO8N2MJ5>
2019-03-05 22:39:34 +0800

    添加类注释

--- springdemo/src/main/java/com/example/demo/DemoApplication.java
+++ springdemo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 类注释
+ * @author Administrator
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
