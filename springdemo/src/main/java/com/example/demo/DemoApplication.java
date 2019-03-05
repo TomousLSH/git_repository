4f13d7cd5cd6b5162953c3348fce2869ee396e59
Administrator <Administrator@9HE79S4JO8N2MJ5>
2019-03-05 22:37:32 +0800

    添加注释

--- springdemo/src/main/java/com/example/demo/DemoApplication.java
+++ springdemo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * 添加注释
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
