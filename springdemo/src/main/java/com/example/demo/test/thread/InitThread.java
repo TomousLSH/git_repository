1485e8092252c339d58cdb5bc1b9e48007b659de
lsh <710261614@qq.com>
2019-03-22 11:52:12 +0800

    线程相关1

---
+++ springdemo/src/main/java/com/example/demo/test/thread/InitThread.java
@@ -0,0 +1,14 @@
+package com.example.demo.test.thread;
+
+/**
+ * @author Administrator
+ * 初始化线程的几种方式
+ */
+public class InitThread {
+	
+	
+	
+    public static void main(String[] args) {
+		
+	}
+}
