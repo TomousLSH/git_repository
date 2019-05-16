18672b4fd9662a6751507a5158af5037ff4d3905
lsh <710261614@qq.com>
2019-05-16 21:37:33 +0800

    master-4
    Signed-off-by: lsh <710261614@qq.com>

--- springdemo/src/main/java/com/example/demo/websocket/MasterWebsocket.java
+++ springdemo/src/main/java/com/example/demo/websocket/MasterWebsocket.java
@@ -4,5 +4,6 @@ public class MasterWebsocket {
     public static void main(String[] args){
        System.out.println("001");
        System.out.println("002");
+       System.out.println("003");
     }
 }
