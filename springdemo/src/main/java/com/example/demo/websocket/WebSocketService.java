2b6c3bf91a34313f3d59dc481e0a701813efe3a2
lsh <710261614@qq.com>
2019-05-16 21:26:06 +0800

    branch001
    Signed-off-by: lsh <710261614@qq.com>

--- springdemo/src/main/java/com/example/demo/websocket/WebSocketService.java
+++ springdemo/src/main/java/com/example/demo/websocket/WebSocketService.java
@@ -3,5 +3,7 @@ package com.example.demo.websocket;
 public class WebSocketService {
     public static void main(String[] args){
         System.out.println("websocket test");
+
+        System.out.println("branch001");
     }
 }
