4a113ab7ed19fee3eadd45a5abd75928ab29e077
lishuaihao <lishuaihao@transinfo.com.cn>
2019-05-27 00:05:31 +0800

    huashuo branch1 3333

---
+++ springdemo/src/main/java/com/example/demo/branchhuashuo/HuaShuoClass2.java
@@ -0,0 +1,8 @@
+package com.example.demo.branchhuashuo;
+
+public class HuaShuoClass2 {
+    public static void main(String[] args){
+        System.out.println("huashuo11111");
+        System.out.println("huashuo22222");
+    }
+}
