dd1411b99b2bb7ef7f5e6dd21d2db83b207bd56d
lishuaihao <lishuaihao@transinfo.com.cn>
2019-07-03 16:21:49 +0800

    java jwt,kong日志发送

---
+++ springdemo/src/main/java/com/example/demo/controller/KongLogSentController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.example.demo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/kong")
+public class KongLogSentController {
+
+    @RequestMapping(method = RequestMethod.POST,value = "/receivelog",produces = "application/json;charset=utf-8")
+    public HashMap receivelog(HttpServletRequest request){
+       // System.out.println(JSON.toJSONString(request));
+
+        System.out.println("接收到日志请求");
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        System.out.println(JSON.toJSONString(parameterMap));
+
+
+        HashMap<Object, Object> hashMap = new HashMap<>();
+        hashMap.put("name", "li");
+        return hashMap;
+    }
+
+}
---
+++ springdemo/src/main/java/com/example/demo/test/jwt/JwtUtil.java
@@ -0,0 +1,54 @@
+package com.example.demo.test.jwt;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+
+    public static void main(String[] args){
+
+        System.out.println(createJWT(30000));
+
+    }
+
+    //参数为token过期时间，单位为毫秒
+    public static String createJWT(long ttlMillis) {
+
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        String secret = "u68S1WzV1668HQfqYmjnZkeBWOyPw58e"; //此处字符串对应consumer中jwt参数的secret
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder()
+                .setIssuer("TBi6JFdoC44Bow69Ee5EOiDJoC5TyM6x") //此处字符串对应consumer中jwt参数的key
+                .setIssuedAt(new Date())
+                .signWith(signatureAlgorithm, secret);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+//        long nbf = nowMillis + 600000;
+//        Date nbfDate = new Date(nbf);
+//        builder.setNotBefore(nbfDate);
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+
+    }
+
+}
